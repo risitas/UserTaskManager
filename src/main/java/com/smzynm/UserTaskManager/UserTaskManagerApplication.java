0f74068583c09b6693ad02ad2eb5a7825fe97807
risitas <yosimarnaranjo9@gmail.com>
2023-07-01 18:48:49 -0500

    project structure

---
+++ src/main/java/com/smzynm/UserTaskManager/UserTaskManagerApplication.java
@@ -0,0 +1,13 @@
+package com.smzynm.UserTaskManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserTaskManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserTaskManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smzynm/UserTaskManager/UserTaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smzynm.UserTaskManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserTaskManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
