24321243dcdd592833d963852c59ab1f76ae49a4
risitas <yosimarnaranjo9@gmail.com>
2023-07-13 13:13:58 -0500

    correption dockerfile and configuration

--- target/generated-sources/annotations/com/smzynm/UserTaskManager/mapper/TeamMapperImpl.java
+++ target/generated-sources/annotations/com/smzynm/UserTaskManager/mapper/TeamMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-07-13T12:12:18-0500",
+    date = "2023-07-13T13:12:29-0500",
     comments = "version: 1.5.3.Final, compiler: javac, environment: Java 17.0.6 (Oracle Corporation)"
 )
 @Component
